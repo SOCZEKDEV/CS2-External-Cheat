51b5a458b9ee693ca4273157f27f79d236cafcc9
SOCZEKDEV <103373053+SOCZEKDEV@users.noreply.github.com>
2025-02-02 17:41:42 +0100

    Update Windows.cs

--- Core/Windows.cs
+++ Core/Windows.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using CS2.Hacks;
 using GameOverlay.Drawing;
@@ -6,11 +7,26 @@ namespace CS2.Core;
 
 public class Windows
 {
+    private static string fullText = "Juice cheats";
+    private static string animatedText = "";
+    private static int animationDuration = 30000; // 30 seconds
+    private static int interval = animationDuration / fullText.Length;
+
+    public static async Task AnimateText()
+    {
+        for (int i = 0; i <= fullText.Length; i++)
+        {
+            animatedText = fullText.Substring(0, i);
+            await Task.Delay(interval);
+        }
+    }
+
     public static void DrawWatermark(Overlay overlay, Graphics gfx, System.Drawing.Point cursorPos)
-	{		
-		var infoText = $"FPS: {gfx.FPS} | Cursor: {cursorPos.X}, {cursorPos.Y}";
-		gfx.DrawTextWithBackground(overlay.fonts["consolas"], overlay.colors["green"], overlay.colors["black"], 10, 10, infoText);
-	}
+    {       
+        var infoText = $"FPS: {gfx.FPS} | Cursor: {cursorPos.X}, {cursorPos.Y}";
+        gfx.DrawTextWithBackground(overlay.fonts["consolas"], overlay.colors["green"], overlay.colors["black"], 10, 10, infoText);
+        gfx.DrawTextWithBackground(overlay.fonts["consolas"], overlay.colors["red"], overlay.colors["black"], 10, 30, animatedText);
+    }
 
     public static async Task UseTriggerBot(Overlay overlay, Graphics gfx, System.Drawing.Point cursorPos)
     {
